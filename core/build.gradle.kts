65196f1db05b4c421c9dc8c164a8258e6981921d
Huseyn Sharifzade <huseynsharif@gmail.com>
2024-05-23 15:18:34 +0400

    add account

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -48,6 +48,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
-    implementation(project(":domain"))
 
 }
