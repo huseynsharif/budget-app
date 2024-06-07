f7a96018404e495672cdfa34cc1bcdc30d0a7584
Huseyn Sharifzade <huseynsharif@gmail.com>
2024-06-07 23:59:49 +0400

    account info

--- uikit/src/main/java/com/huseynsharif/uikit/AccountInfoBottomSheet.kt
+++ uikit/src/main/java/com/huseynsharif/uikit/AccountInfoBottomSheet.kt
@@ -6,15 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.huseynsharif.domain.entities.Account
+import com.huseynsharif.uikit.databinding.FragmentAccountInfoBottomSheetBinding
 
 class AccountInfoBottomSheet(
     private val account: Account
 ) : BottomSheetDialogFragment() {
 
+    private lateinit var binding: FragmentAccountInfoBottomSheetBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account_info_bottom_sheet, container, false)
+        binding = FragmentAccountInfoBottomSheetBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            name.text = account.name
+            type.text = account.type.name
+            currency.text = account.currency
+            balance.text = account.amount.toString()
+        }
     }
 }
